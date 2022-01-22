d60655c8bb6873f7f7fec4340ed7754c0c823176
Diego Rocha <dkxever@gmail.com>
2022-01-22 15:49:12 -0300

    Adding dispatch of useReducer and provide variables for app

--- src/GlobalContext.jsx
+++ src/GlobalContext.jsx
@@ -4,6 +4,8 @@ import reducer from './reducer'
 
 export const AppContext = createContext();
 
+// const url = "endereço";
+
 const initialState = {
   loading: false,
   cart: cartItems,
@@ -15,8 +17,42 @@ export const AppProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
+// Ao executar a função, disparamos para nosso action um type e um id que recebemos de CartItem
+
+const clearCart = () => {
+  dispatch({ type:"CLEAR_CART" })
+}
+
+const remove = (id) => {
+  dispatch({ type:"REMOVE", payload: id })
+}
+
+const increase = (id) => {
+  dispatch({ type:"INCREASE", payload: id })
+}
+
+const decrease = (id) => {
+  dispatch({ type:"DECREASE", payload: id })
+}
+
+useEffect( async () => {
+  dispatch({ type:"LOADING" })
+  /* API */
+  // const response = await fetch(url);
+  // const cart = await response.json();
+  dispatch({ type:"DISPLAY_ITEMS", payload: state.cart, loading: false })
+},[])
+
+useEffect(() => {
+  dispatch({ type:"GET_TOTALS" })
+},[state.cart])
+
+const toggleAmount = (id, type) => {
+  dispatch({ type:"TOGGLE_AMOUNT", payload:{ id, type } })
+}
+
     return(
-        <AppContext.Provider value={{}}>
+        <AppContext.Provider value={{ ...state, clearCart, remove, increase, decrease, toggleAmount }}>
             {children}
         </AppContext.Provider>
     )
