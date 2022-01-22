96d5e437c772dce968f608935e04ba208baf8710
Diego Rocha <dkxever@gmail.com>
2022-01-22 15:51:57 -0300

    Create home app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import { useGlobalContext } from './GlobalContext'
+import CartContainer from './Components/CartContainer'
 
 const App = () => {
-  return <div>Olá Mundo</div>
+
+    const { loading } = useGlobalContext();
+
+    if(loading){
+        return(
+            <div>Loading...</div>
+        )
+    }
+
+    return (
+        <main>
+            <Navbar/>
+            <CartContainer/>
+        </main>
+    )
 }
 
 export default App
